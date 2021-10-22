f31e09656a6b5299d71350245087dfdab2a3f63c
Reinaart van Loon <vanloon.reinaart@gmail.com>
2021-10-22 14:36:57 +0200

    Update python file

--- python_analysis.py
+++ python_analysis.py
@@ -27,7 +27,7 @@ def t_sec(time):
 #Find index of data corresponding to t in array named times [seconds]
 def i_time(t,times):
     times = [int(time) for time in times]
-    print(times)
+    
     try: 
         i = times.index(int(t))
     except:
@@ -50,15 +50,20 @@ def data_retr(df,t_end):
     i_start = i_time((t_sec(t_end)-60),times)
     i_end = i_time(t_sec(t_end),times)
     
-    print(i_end)
+
     
     #Compute ratio as timeseries for manhole measurement
-    ratio = df.CH4[i_start:i_end+1]/df.CO2[i_start:i_end+1]
-    ratio_dry = df.CH4_dry[i_start:i_end+1]/df.CO2_dry[i_start:i_end+1]
+    ratio = df.CH4[i_start:i_end]/df.CO2[i_start:i_end]
+    ratio_dry = df.CH4_dry[i_start:i_end]/df.CO2_dry[i_start:i_end]
     
-    print(ratio)
+    ratio_avg = np.average(ratio.to_numpy())
+    ratio_dry_avg = np.average(ratio_dry.to_numpy())
+
+    return ratio_avg, ratio_dry_avg    
+
     
 
+#%%Import data
 
 #Script runs if directories with data are places in same folder as this python file
 path = os.path.dirname(__file__)
@@ -80,22 +85,60 @@ df4 = pd.read_csv(os.path.join(path, "G2301\\15\\CFADS2394-20211015-155955Z-Data
                   sep="\s+")
 
 df = pd.concat([df1, df2, df3, df4], ignore_index=True)
-print(df["TIME"])
 
 # The manholes numbering/naming
 manholes = np.arange(1,21)
 
 # End times of each measurement to retrieve the data coresponding to the manholes
 # Use notation 13:10:05 for 5 seconds past 10 minutes past 13h. 
-t_end = '15:30:00'
-
-plt.plot(df["TIME"])
-
-#data_retr(df,t_end)
+t_end = ['15:14:00' for i in range(len(manholes))]  #Change to actual times!!
 
 
+#Import coordinates
+coor = pd.read_csv(os.path.join(path,"coordinates.csv"),delimiter=';')
 
-#data = pd.DataFrame(data={'manholes':manholes,'end times':t_end},index=manholes)
+#%%Analyse data
+
+
+#Retrieve data and append to lists
+ratios = []
+dry_ratios = []
+for i in range(len(t_end)):
+    t = t_end[i]
+    ratio, ratio_dry = data_retr(df,t)
+    ratios.append(ratio)
+    dry_ratios.append(ratio_dry)
+
+#Build dataframe with data
+data = pd.DataFrame(data={'manholes':manholes,'end times':t_end,'ratio':ratios,'dry ratio':dry_ratios },index=manholes)
+
+#Save data to csv file
+data.to_csv(os.path.join(path,'data'),index=False)
+
+#%%Plotting data
+
+import matplotlib.pyplot as plt
+
+lat_max =52.0881
+lat_min =52.0847   
+
+lon_max = 5.1775
+lon_min = 5.1632
+
+BBox = ((lon_min,lon_max,lat_min,lat_max))
+im = plt.imread(os.path.join(path,"map_52.0881_5.1775_52.0847_5.1632.png"))
+
+fig,ax = plt.subplots(figsize=(12,8))
+
+ax.scatter(coor['long'],coor['lat'],alpha=0.8,zorder=1,s=40)
+ax.set_xlim(lon_min,lon_max)
+ax.set_ylim(lat_min,lat_max)
+
+ax.imshow(im,zorder=0,extent=BBox,aspect='equal',alpha=0.5)
+
+
+
+
 
 
 
