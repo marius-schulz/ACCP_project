47f25ae414a78c8f5e1c91dadb02a65f18b598b5
Reinaart van Loon <vanloon.reinaart@gmail.com>
2021-10-26 11:47:05 +0200

    Python update without background
    Removed background levels and rendered new plots

--- python_analysis.py
+++ python_analysis.py
@@ -93,18 +93,12 @@ t_end = timestamps["END"]
 #Import coordinates
 coor = pd.read_csv(os.path.join(path,"coordinates.csv"),delimiter='\t')
 
-#Background levels CH4 and CO2 [ppm]
-CH4_bg = 1.97 
-CO2_bg = 403
+#Background levels CH4 and CO2 [ppm] for the dry measurements
+CH4_bg = 1.99
+CO2_bg = 413
 
-u_CH4_bg = 0.04
-u_CH6_bg = 3
-
-CH4_dry_bg = 1.99
-CO2_dry_bg = 413
-
-u_CH4_dry_bg = 0.002
-u_CO2_dry_bg = 8
+u_CH4_bg = 0.002
+u_CO2_bg = 8
 
 #%%Make Regression Fit
 
@@ -115,8 +109,8 @@ def lin_reg(i,plot): #Defining function that gives fit parameters for manhole i
     times = [t_sec(time)+7225 for time in df["TIME"]] #Get time corresponding to manhole measurement
     i_start = i_time((t_sec(t_end[i])-50),times)
     i_end = i_time(t_sec(t_end[i]),times)
-    xdata = df.CO2_dry[i_start:i_end]-403 #Calculate measured CO2 excess concentrations
-    ydata = df.CH4_dry[i_start:i_end]-1.97 #Calculate measured CH4 excess concentrations
+    xdata = df.CO2_dry[i_start:i_end]-CO2_bg #Calculate measured CO2 excess concentrations
+    ydata = df.CH4_dry[i_start:i_end]-CH4_bg #Calculate measured CH4 excess concentrations
     popt, pcov = curve_fit(func, xdata, ydata) #make fit
     if plot: #optional: Scatter plot concentrations and fitted function
         plt.scatter(xdata,ydata)
@@ -133,56 +127,34 @@ for i in range(len(t_end)):
     params[1].append(popt[1])
 print(params[0])#print regression slope values for all manholes
 plt.scatter(range(len(params[0])),params[0])
-#%%Analyse data
-
-#compute background ratios
-ratio_bg = CH4_bg/CO2_bg
-ratio_dry_bg = CH4_dry_bg/CO2_dry_bg
-
-#Retrieve data and append to lists
-ratios = []
-dry_ratios = []
-
-for i in range(len(t_end)):
-    t = t_end[i]
-    ratio, ratio_dry = data_retr(df,t)
-    ratios.append(ratio)
-    dry_ratios.append(ratio_dry)
-    
-    
-    
-#Remove background ratios from results and convert to promille
-ratios_norm = (np.array(ratios)-ratio_bg)*100
-ratios_dry_norm = (np.array(dry_ratios)-ratio_dry_bg)*100
-
 
 
 #%%Plotting data
 
-from mpl_toolkits.axes_grid1 import make_axes_locatable
+# from mpl_toolkits.axes_grid1 import make_axes_locatable
 
 
-lat_max =52.0881
-lat_min =52.0847   
+# lat_max =52.0881
+# lat_min =52.0847   
 
-lon_max = 5.1775
-lon_min = 5.1632
+# lon_max = 5.1775
+# lon_min = 5.1632
 
-BBox = ((lon_min,lon_max,lat_min,lat_max))
-im = plt.imread(os.path.join(path,"map_52.0881_5.1775_52.0847_5.1632.png"))
+# BBox = ((lon_min,lon_max,lat_min,lat_max))
+# im = plt.imread(os.path.join(path,"map_52.0881_5.1775_52.0847_5.1632.png"))
 
-fig,ax = plt.subplots(figsize=(12,8))
+# fig,ax = plt.subplots(figsize=(12,8))
 
-plot = ax.scatter(coor['long'],coor['lat'],alpha=0.8,zorder=1,s=40, c=ratios_norm,cmap='plasma')
-ax.set_xlim(lon_min,lon_max)
-ax.set_ylim(lat_min,lat_max)
+# plot = ax.scatter(coor['long'],coor['lat'],alpha=0.8,zorder=1,s=40, c=ratios_norm,cmap='plasma')
+# ax.set_xlim(lon_min,lon_max)
+# ax.set_ylim(lat_min,lat_max)
 
-ax.imshow(im,zorder=0,extent=BBox,aspect='equal',alpha=0.5)
+# ax.imshow(im,zorder=0,extent=BBox,aspect='equal',alpha=0.5)
 
-divider = make_axes_locatable(ax)
-cax = divider.append_axes("right", size="2%", pad=0.05)
-cbar = plt.colorbar(plot, cax=cax)
-cbar.set_label("ppm/ppm %")
+# divider = make_axes_locatable(ax)
+# cax = divider.append_axes("right", size="2%", pad=0.05)
+# cbar = plt.colorbar(plot, cax=cax)
+# cbar.set_label("ppm/ppm %")
 
 
 
@@ -191,7 +163,6 @@ cbar.set_label("ppm/ppm %")
 #Build dataframe with data
 data = pd.DataFrame(data={'manholes':manholes,'end times':t_end,'ratio':ratios,'dry ratio':dry_ratios },index=manholes)
 data.to_csv(os.path.join(path,'data'),index=False)
-
 
 
 #%%Make plots of all times of interest (ToI), times at which the data is selected
@@ -236,8 +207,8 @@ for i in range(len(t_end)):
     #Plot data little before and after ToI
     overshoot = 30 #Amount of datapoints overshoot before and after time of interest
     times_plot = times[int(i_start-overshoot):int(i_end+overshoot)] #Timespan that is plotted
-    CH4 = df['CH4'][int(i_start-overshoot):int(i_end+overshoot)] #Select data
-    CO2 = df['CO2'][int(i_start-overshoot):int(i_end+overshoot)] #Select data
+    CH4 = df['CH4_dry'][int(i_start-overshoot):int(i_end+overshoot)]-CH4_bg #Select data
+    CO2 = df['CO2_dry'][int(i_start-overshoot):int(i_end+overshoot)]-CO2_bg #Select data
     
     CH4_plot = axs[0].plot(times_plot,CH4)
     C02_plot = axs[1].plot(times_plot,CO2)
@@ -284,8 +255,8 @@ undershoot = 100 #Amount of datapoints overshoot before and after time of intere
 overshoot = 100 #Amount of datapoints overshoot before and after time of interest
 
 times_plot = times[int(i_start-undershoot):int(i_end+overshoot)] #Timespan that is plotted
-CH4 = df['CH4'][int(i_start-undershoot):int(i_end+overshoot)] #Select data
-CO2 = df['CO2'][int(i_start-undershoot):int(i_end+overshoot)] #Select data
+CH4 = df['CH4'][int(i_start-undershoot):int(i_end+overshoot)]-CH4_bg #Select data
+CO2 = df['CO2'][int(i_start-undershoot):int(i_end+overshoot)]-CO2_bg #Select data
 
 CH4_plot = axs[0].plot(times_plot,CH4)
 C02_plot = axs[1].plot(times_plot,CO2)
